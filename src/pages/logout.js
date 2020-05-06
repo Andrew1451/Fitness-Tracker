edb42b348970cbf743b2b299cd568773e65ce419
Andrew <andrew.gaynor@ymail.com>
2020-05-06 12:04:05 -0400

    put redirect in useEffect for logout

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { connect } from "react-redux"
 import * as actions from "../store/actions/index"
 import { navigate } from "gatsby";
 
 const Logout = props => {
-    props.onLogout();
-    navigate('/');
+    const { onLogout } = props;
+    useEffect(() => {
+        onLogout();
+        navigate('/');
+    }, [ onLogout ])
     return <div></div>
 }
 

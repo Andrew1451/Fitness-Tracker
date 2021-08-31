ee8da857955ee3e5cf1e136772f18e60a1b3cce9
Andrew <andrew.gaynor@ymail.com>
2021-08-31 12:22:43 -0400

    add testing-library/user-event and test Cancel button on signupForm

--- src/containers/signupForm.test.js
+++ src/containers/signupForm.test.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from '../test-utils'
+import userEvent from '@testing-library/user-event'
 import SignupForm from './signupForm'
 
 describe('SignupForm', () => {
     test('when user clicks cancel, the form inputs should be cleared', () => {
-        const { getByRole } = render(<SignupForm />)
-        expect(getByRole('textbox')).toBeInTheDocument()
+        const { getByRole, getByText, debug } = render(<SignupForm />)
+        const input = getByRole('textbox')
+        userEvent.type(input, 'example@email.com')
+        expect(input).toHaveValue('example@email.com')
+        userEvent.click(getByText('Cancel'))
+        expect(input).toHaveValue('')
     })
 })
